c6c12e0e63b98dd5caa9f062d08a894dd157aa51
Jd Ford <19841889+jdford07@users.noreply.github.com>
2023-03-09 22:57:10 -0600

    First working local FastAPI requests

--- osrsNewsAPI.py
+++ osrsNewsAPI.py
@@ -1,2 +1,14 @@
 #fastAPI here
 from fastapi import FastAPI
+import mongoDBRequests
+import osrsNewsCore
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Hello":"World"}
+
+@app.get("/api/v1/MainNewsArticles")
+def mainArticles():
+    return osrsNewsCore.getOSRSMainPageNewsArticles()
